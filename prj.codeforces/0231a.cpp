1e5bd273ebd7ac03b56bf4675683092ad8ca2ab3
Grigoriy <61056244+domster704@users.noreply.github.com>
2023-10-29 20:37:58 +0300

    Update 0231a.cpp

--- prj.codeforces/0231a.cpp
+++ prj.codeforces/0231a.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+int main() {
+    int n;
+    int sum = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        sum += a + b + c >= 2 ? 1 : 0;
+    }
+    std::cout << sum;
+    return 0;
+}
